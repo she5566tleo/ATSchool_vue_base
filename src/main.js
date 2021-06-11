5acf4c29b6971dff7851ee146618feaf4ab29ba5
TSAI YU-LIANG <she5566tleo@gmail.com>
2021-06-11 14:34:20 +0800

    base framework install

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
+import vuetify from './plugins/vuetify'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.use(vuetify)
+
+app.mount('#app')
